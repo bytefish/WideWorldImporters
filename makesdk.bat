3c37a8b9224f7854492212d6ef2bd40fe4e33491
Philipp Wagner <pwag86@googlemail.com>
2023-11-11 20:13:36 +0100

    Restructure Project

---
+++ makesdk.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+:: Kiota Executable
+set KIOTA_EXECUTABLE=kiota
+
+:: Parameters for the Code Generator
+set PARAM_OPENAPI_SCHEMA="https://localhost:5000/odata/openapi.json"
+set PARAM_LANGUAGE=csharp
+set PARAM_NAMESPACE=WideWorldImporters.Shared.ApiSdk 
+set PARAM_OUT_DIR=%~dp0/src/Shared/WideWorldImporters.Shared.ApiSdk
+set PARAM_LOG_LEVEL=Trace
+
+:: Run the "kiota generate" Command
+%KIOTA_EXECUTABLE% generate^
+    --openapi %PARAM_OPENAPI_SCHEMA%^
+    --language %PARAM_LANGUAGE%^
+    --namespace-name %PARAM_NAMESPACE%^
+    --log-level %PARAM_LOG_LEVEL%^
+    --output %PARAM_OUT_DIR%
