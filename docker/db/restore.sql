d2528586c9a5efee8995732745dd985bfed7c90a
Philipp Wagner <philipp@bytefish.de>
2024-04-22 12:00:07 +0800

    Update restore.sql

--- docker/db/restore.sql
+++ docker/db/restore.sql
@@ -3,8 +3,6 @@ RESTORE DATABASE WideWorldImporters
     WITH 
         MOVE "WWI_Primary" TO "/var/opt/mssql/data/WideWorldImporters.mdf",
         MOVE "WWI_Userdata" TO "/var/opt/mssql/data/WideWorldImporters_UserData.ndf",
-        MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "WWI_InMemory_Data_1"
-    TO 
-        "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"
-
+        MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", 
+        MOVE "WWI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"
 GO
